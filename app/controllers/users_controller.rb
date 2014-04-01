a771a555e718e0ca76c56fa271b0443d79dea4d5
timkutnick <tim.kutnick@gmail.com>
2014-04-01 08:47:36 -0700

    Fix users require_current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :require_no_current_user!, :only => [:new, :create]
+  before_filter :require_current_user!, :only => [:show]
   
   def new
     @user = User.new
